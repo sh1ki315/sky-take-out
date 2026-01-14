ed303701ba38afb320ade930795ded19add32c2b
sh1k315 <Wngkau6@126.com>
2026-01-14 18:48:55 +0800

    員工分頁查詢業務代碼開發

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -15,8 +17,12 @@ public interface EmployeeService {
     /**
      * 新增員工
      * @param employeeDTO
-     * @return
      */
     void save(EmployeeDTO employeeDTO);
-
+    /**
+     * 分頁查詢
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
